23254170d8eabcbb666d77e1b2464562e4e3ea65
Miruna Radu <miruna.radu@p3-group.com>
2019-04-22 14:53:42 +0300

    Add dummy bottom navigation menu.

--- app/src/main/java/com/example/test/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/test/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.test.ui.main
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.test.R
@@ -9,6 +11,7 @@ import com.example.test.data.model.Item
 import com.example.test.observeNonNull
 import com.example.test.ui.adapter.ItemsAdapter
 import com.example.test.ui.list.ListFragment
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<MainViewModel, MainViewModelFactory>(){
@@ -24,8 +27,29 @@ class MainActivity : BaseActivity<MainViewModel, MainViewModelFactory>(){
         supportFragmentManager.beginTransaction().add(R.id.fragment_container,
                 ListFragment.newInstance()).commit()
         setUpListeners()
+        navigation_bar.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.home_navigation->{
+                    Toast.makeText(this,"Home reclicked!",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        navigation_bar.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home_navigation->{
+                    Toast.makeText(this,"Home clicked!",Toast.LENGTH_SHORT).show()
+                    Log.d("LALALA","Clicked")
+                    true}
+                R.id.music_navigation->{
+                    Toast.makeText(this,"Music clicked",Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
 
     }
+
 
     private fun setUpListeners() {
         addItemButton.setOnClickListener {
